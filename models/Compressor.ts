1a01fec3b85426be408015a4205b0362f57ec718
Manav Desai <manav.p.desai@gmail.com>
2023-10-30 01:47:30 +0530

    feat: added function to compress and write header and data to a file

--- models/Compressor.ts
+++ models/Compressor.ts
@@ -13,17 +13,19 @@ export class Compressor {
     public process(): this{
         const map = new Map();
         const stream  = fs.createReadStream(process.cwd() + `/data/${this.fileName}`);
+        let text = '';
         stream.on("data", (chunk)=>{
+            text+=chunk
             this.countCharacters(chunk.toString(), map);
         })
         stream.on("close", ()=>{
-            this.initialiseHuffmanTree(map);
+            this.initialiseHuffmanTree(map, text);
         })
 
         return this;
     }
 
-    private initialiseHuffmanTree(map: Map<string, number>){
+    private initialiseHuffmanTree(map: Map<string, number>, text: string){
         const minPriorityQueue = new FastPriorityQueue((node1: HuffmanTree, node2: HuffmanTree)=>{
             let weight1 = node1.weight();
             let weight2 = node2.weight();
@@ -35,21 +37,51 @@ export class Compressor {
             minPriorityQueue.add(leafNode);
         }
         this.lookupTable = HuffmanTree.buildTree(minPriorityQueue);
+        this.compressAndWrite(this.lookupTable, text);
 
-        for(let [key,value] of this.lookupTable?.entries()!){
-            console.log(`${key}:`, value)
-        }
     }
 
     private countCharacters(chunk: string, map: Map<string, number>){
-        const characters = chunk.split("");
-        for(let character of characters){
-            if(map.has(character)){
-                map.set(character, map.get(character)!+1);
-            }else{
-                map.set(character, 1);
-            }
+        for(let i=0; i<chunk.length; i++){
+            let character = chunk.charAt(i);
+            map.set(character, (map.get(character) ?? 0)+1)
         }
+    }
+
+    private compressAndWrite(lookupTable: Map<string, string>| null, text:string){
+        let header = JSON.stringify(Array.from(lookupTable!.entries()));
+        const headerUint8Array = new TextEncoder().encode(header);
+        
+        const path = process.cwd()+"/data/compressed.txt";
+        const textBits = text.split('').map((char)=>lookupTable?.get(char)).join('');
+        const [compressedTextUint8, padding] = this.getUint8TextArray(textBits);
+        fs.writeFileSync(path, headerUint8Array.length.toString() + '\n');
+        fs.appendFileSync(path, padding.toString()+'\n');
+        fs.appendFileSync(path, headerUint8Array);
+        fs.appendFileSync(path, compressedTextUint8);
+
+
+    }
+
+    private getUint8TextArray(textBits: string): [Uint8Array, number]{
+        let bits = textBits;
+
+        while(bits.length%8!==0){
+            bits+='0';
+        }
+
+        const bytes = bits.length / 8;
+        const padding = bits.length - textBits.length;
+        let bitsArray = new Uint8Array(bytes);
+
+        for(let index=0; index<bits.length; index++){
+            const start = index*8;
+            const end = start + 8;
+            const text = bits.slice(start, end);
+            bitsArray[index] = parseInt(text, 2);
+        }
+
+        return [bitsArray, padding];
     }
 
 }
